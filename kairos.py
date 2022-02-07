b42d90b329c237721e1d0733cfaa0e8177a9dd7e
PatrickDalasenIII <99158569+PatrickDalasenIII@users.noreply.github.com>
2022-02-07 13:13:49 +0800

    Update kairos.py

--- kairos.py
+++ kairos.py
@@ -13,6 +13,7 @@ import nltk
 from nltk import pos_tag
 from nltk.corpus import stopwords
 nltk.download('wordnet')
+nltk.download('omw-1.4')
 pos_dict = {'J':wordnet.ADJ, 'V':wordnet.VERB, 'N':wordnet.NOUN, 'R':wordnet.ADV}
 
 def token_stop_pos(text):
